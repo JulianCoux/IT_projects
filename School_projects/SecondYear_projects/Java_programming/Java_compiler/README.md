a2ec87b26aa885c03ddc259698c183953bfcc541
JulianCoux <155066303+JulianCoux@users.noreply.github.com>
2024-01-29 11:52:07 +0100

    Delete School_projects/SecondYear_projects/Java_programming/Java_compiler directory

--- School_projects/SecondYear_projects/Java_programming/Java_compiler/README.md
+++
@@ -1,2 +0,0 @@
-# Projet Génie Logiciel, Ensimag.
-gl25, 01/01/2024.
--- School_projects/SecondYear_projects/Java_programming/Java_compiler/src/main/java/fr/ensimag/deca/tools/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Utilities used in various stages of the Deca compiler.
- *
- * @author gl25
- * @date 01/01/2024
- */
-package fr.ensimag.deca.tools;
